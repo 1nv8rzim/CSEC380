929b05dba94d3e22ea9fc7a2517cbff9ee8c5ec7
Max <msf9542@rit.edu>
2022-01-24 18:24:34 -0500

    hopefully working

--- HW1/ACT4/step2/act4ste2.py
+++ HW1/ACT4/step2/act4ste2.py
@@ -1,7 +1,7 @@
-from requests import get
 from argparse import ArgumentParser
 from ipaddress import IPv4Address
 from threading import Thread
+from proxy_checker import ProxyChecker
 
 
 class Scanner:
@@ -9,16 +9,16 @@ class Scanner:
         self.args = ArgumentParser()
         self.args.add_argument('start_ip', help="Starting IP of proxy scanner")
         self.args.add_argument('end_ip', help="Ending IP of proxy scanner")
-        self.args.add_argument('-t', '--threads', default=10,
-                               type=int, help="Number of threads to run the Scanner")
-        self.args.add_argument('-a', '--all-ports', default=False,
+        self.args.add_argument('-t', '--threads', default=2,
+                               type=int, help="Number of threads to run the Scanner. WARNING: going above 5 threads results in false negatives")
+        self.args.add_argument('-p', '--ports', default=False,
                                action='store_true', help='Use uncommon proxy ports too')
         self.args = self.args.parse_args()
 
         self.threads = []
-        self.common_ports = (80, 443, 808, 1080, 3128, 8080, 8118)
+        self.common_ports = (80, 443, 808, 1080, 3128, 8080, 8118, 8888)
         self.uncommon_ports = (81, 82, 83, 84, 85, 86, 88, 1337, 3124, 3127, 3129, 6515, 6588,
-                               6666, 6675, 8000, 8001, 8008, 8081, 8082, 8085, 8088, 8090, 8123, 8800, 8888, 8909, 9000, 9415, 36081, 54321, 60099)
+                               6666, 6675, 8000, 8001, 8008, 8081, 8082, 8085, 8088, 8090, 8123, 8800, 8909, 9000, 9415, 36081, 54321, 60099)
         self.main()
 
     def get_address(self):
@@ -26,7 +26,7 @@ class Scanner:
         for address in range(int(IPv4Address(self.args.start_ip)), int(IPv4Address(self.args.end_ip))):
             for port in self.common_ports:
                 yield str(IPv4Address(address)) + f':{port}'
-        if self.args.a:
+        if self.args.ports:
             print('[+] Checking uncommon ports')
             for address in range(int(IPv4Address(self.args.start_ip)), int(IPv4Address(self.args.end_ip))):
                 for port in self.uncommon_ports:
@@ -40,6 +40,10 @@ class Scanner:
             self.threads.append(self.ScannerThread(generator))
             self.threads[-1].start()
         self.threads[-1].join()
+
+        for thread in self.threads:
+            if thread.is_alive():
+                thread.join()
 
         proxies = []
 
@@ -58,13 +62,13 @@ class Scanner:
 
             self.generator = generator
             self.proxies = []
+            self.checker = ProxyChecker()
 
         def run(self):
             while (address := next(self.generator)) is not None:
-                request = get("https://csec.rit.edu",
-                              proxies={"http": address})
-                if request.status_code == 200:
-                    self.proxies += address
+                test = self.checker.check_proxy(address)
+                if test != False:
+                    self.proxies.append(address)
 
 
 Scanner()
